b1cd8b7d60a250907adca768b8e98de51f0f9c2d
Nancy Thomas <nancytreasathomas@gmail.com>
2024-06-23 17:35:30 +0530

    Handles Project Creation & Updating UI

--- src/App.jsx
+++ src/App.jsx
@@ -20,18 +20,19 @@ function App() {
 
   function handleAddProject(projectData){
     setProjectsState(prevState => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random()
+        id: projectId,
       };
       return{
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject]
       }
     })
   }
-  console.log(projectsState);
-  
+
   let content;
 
   if(projectsState.selectedProjectId === null){
@@ -41,7 +42,7 @@ function App() {
   }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProjects}/>
+      <ProjectsSidebar onStartAddProject={handleStartAddProjects} projects={projectsState.projects}/>
       {content}
     </main>
   );
