8a47590d8b7ff618401ca4bf7b0caa13e5984845
Nancy Thomas <nancytreasathomas@gmail.com>
2024-06-23 15:46:32 +0530

    Adding & styling Sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,25 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
-
+// from tailwindcss website
 
 // /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,ts,jsx,tsx}",
-//   ],
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
 //   theme: {
 //     extend: {},
 //   },
 //   plugins: [],
 // }
+
+
+//from Udemy Instructor
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
